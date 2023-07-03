739221022c4c4c7c835ee015f611aed3a4dada21
swan-Ryu <rsy7942@gmail.com>
2023-07-03 23:22:08 +0900

    command summary, local to remote

--- python_test.py
+++ python_test.py
@@ -5,5 +5,34 @@
 
 print("Hello, world")
 print("We will start git-hub. We can do it.")
-
 print("Hey big brothers, Let's do it together.")
+
+# %%
+
+### 정보 입력하기
+# git config -global user.name "name"
+# git config -global user.email "xxxx@xxxxx.com"
+
+### Git 초기화
+# git init
+
+### 
+# git status
+
+### 
+# git add . # 전체 파일 staging area에 올리기
+
+### commit 하기
+# git commit -m "xxxx"
+
+### commit log 확인하기
+# git log
+
+### local을 remote와 연결하기
+# git remote add origin [git hub 주소]
+
+### remote 확인
+# git remote -v
+
+### remote의 master에 push 하기
+# git push origin master
